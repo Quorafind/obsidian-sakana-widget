8996f60acb90334027343d25bf2ff5655818ddaf
Quorafind <bonopengate@gmail.com>
2022-10-29 16:51:15 +0800

    FIXED: Add obsidian type support

--- sakanaTypes.d.ts
+++ sakanaTypes.d.ts
@@ -2,3 +2,14 @@ interface SakanaWidgetInterface {
 	name: string,
 	url: string,
 }
+
+declare module "obsidian" {
+	interface App {
+		plugins: {
+			enabledPlugins: Set<string>;
+			plugins: {
+				[id: string]: any;
+			};
+		}
+	}
+}
